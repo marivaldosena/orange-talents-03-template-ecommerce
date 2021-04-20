9566251c88f38a8249966abf057d62ea81178fb7
Marivaldo Sena <marivaldo.sena@live.com>
2021-04-20 15:25:58 -0300

    #mercado-livre: Implement request validation for user's opinion regarding products.

---
+++ src/main/java/com/github/marivaldosena/mercadolivre/opinions/AddOpinionRequest.java
@@ -0,0 +1,39 @@
+package com.github.marivaldosena.mercadolivre.opinions;
+
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class AddOpinionRequest {
+    @NotNull
+    @NotEmpty
+    private String title;
+
+    @NotNull
+    @Range(min = 1, max = 5)
+    private String review;
+
+    @NotNull
+    @Size(min = 3, max = 500)
+    private String description;
+
+    public AddOpinionRequest(String title, String review, String description) {
+        this.title = title;
+        this.review = review;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
